fa000ce9cff2ef50ddcf4bac5fcf90dfef7c71a9
Ehsan Fallahi <ehsanfallahi@gmail.com>
2021-01-04 10:33:54 +0330

    modify navigation component

---
+++ app/src/main/java/com/ehsanfallahi/farsnews/ui/detail/NewsDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.ehsanfallahi.farsnews.ui.detail
+
+import androidx.lifecycle.ViewModel
+
+class NewsDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/ehsanfallahi/farsnews/ui/list/NewsListViewModel.kt
@@ -0,0 +1,7 @@
+package com.ehsanfallahi.farsnews.ui.list
+
+import androidx.lifecycle.ViewModel
+
+class NewsListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
